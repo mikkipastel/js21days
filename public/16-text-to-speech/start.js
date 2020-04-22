2c94bdcb5f1dc5933d1de866ef38758605583006
Monthira Chayabanjonglerd <monthira@ookbee.com>
2020-04-22 14:20:56 +0700

    update for day 16 - 17

--- public/16-text-to-speech/start.js
+++ public/16-text-to-speech/start.js
@@ -1,4 +1,25 @@
 (() => {
   // เริ่มเขียนโค้ด
+  const message = new SpeechSynthesisUtterance();
+
+  function onVoicesChanged() {
+    const voices = speechSynthesis.getVoices();
+    const thVoice = voices.find(voice => voice.lang === 'th-TH');
+    message.voice = thVoice;
+  }
+
+  function onclick(event) {
+    message.text = (event.target).getAttribute('alt');
+    speechSynthesis.speak(message);
+  }
+
+  function run() {
+    speechSynthesis.addEventListener('voiceschanged', onVoicesChanged);
+
+    const imageElements = Array.from(document.querySelectorAll('img'));
+    imageElements.forEach(imageElement => imageElement.addEventListener('click', onclick));
+  }
+
+  run();
 
 })();
--- public/17-speech-to-text/start.js
+++ public/17-speech-to-text/start.js
@@ -1,3 +1,45 @@
 (() => {
   // เริ่มเขียนโค้ด
+  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  const recognition = new window.SpeechRecognition();
+  const buttonElement = document.querySelector('.control');
+
+  function onResult(event) {
+    const textElement = document.querySelector('.text');
+    const { transcript } = event.results[0][0];
+    textElement.innerText += transcript;
+  }
+
+  function onEnd() {
+    const isRecording = buttonElement.classList.contains('pause');
+
+    if (isRecording) {
+      recognition.start();
+    }
+  }
+
+  function onClick() {
+    const ifPausing = buttonElement.classList.contains('record');
+
+    if (ifPausing) {
+      recognition.start();
+      buttonElement.classList.remove('record');
+      buttonElement.classList.add('pause');
+    } else {
+      recognition.stop();
+      buttonElement.classList.remove('pause');
+      buttonElement.classList.add('record');
+    }
+  }
+ 
+  function run() {
+    recognition.lang = 'th-TH';
+    recognition.addEventListener('result', onResult);
+    recognition.addEventListener('end', onEnd);
+
+    buttonElement.addEventListener('click', onClick);
+  }
+
+  run();
 })();
