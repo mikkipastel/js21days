1c0617b316315f4aac732728f93bbcf420ef6b2f
Monthira Chayabanjonglerd <monthira@ookbee.com>
2020-04-23 14:18:11 +0700

    add day 19 and fix for mobile site

--- public/19-autocomplete/start.js
+++ public/19-autocomplete/start.js
@@ -1,3 +1,51 @@
 (() => {
   // เริ่มเขียนโค้ด
+  const carBrands = [
+    'BMW',
+    'Masserati',
+    'Mercedes Benz',
+    'Ferrari',
+    'Toyota',
+    'Honda',
+    'Hyundai'
+  ];
+
+  const searchElement = document.querySelector('.search');
+
+  function onInput(event) {
+    clearResults();
+
+    const inputText = event.target.value.toLowerCase();
+    const matchedCarBrands = carBrands.filter(carBrand => carBrand.toLowerCase().startsWith(inputText));
+
+    const ulElement = document.createElement('ul');
+    ulElement.classList.add('results');
+
+    matchedCarBrands.forEach(carBrand => {
+      const liElement = document.createElement('li');
+      liElement.innerText = carBrand
+      liElement.onclick = selectCarBrand;
+      ulElement.appendChild(liElement);
+    });
+    document.body.appendChild(ulElement);
+  }
+
+  function selectCarBrand(event) {
+    searchElement.value = event.target.innerText;
+    clearResults();
+  }
+
+  function clearResults() {
+    const ulElement = document.querySelector('.results');
+    if (ulElement) {
+      document.body.removeChild(ulElement);
+    }
+  }
+
+  function run() {
+    searchElement.addEventListener('input', onInput);
+    document.addEventListener('click', clearResults);
+  }
+
+  run();
 })();
