97fac389f6baed631ed5224efe9e8f279b9cd53a
Monthira Chayabanjonglerd <monthira@ookbee.com>
2020-04-26 14:35:36 +0700

    add day 20 -21

--- public/20-form-validation/start.js
+++ public/20-form-validation/start.js
@@ -1,3 +1,67 @@
 (() => {
   // เริ่มเขียนโค้ด
+  function displayError(element, message) {
+    const smallElement = element.parentElement.querySelector('small');
+    smallElement.innerText = message;
+    element.classList.add('invalid');
+    form.classList.add('invalid');
+  }
+
+  function displaySuccess() {
+    document.body.innerHTML = '';
+
+    const pElement = document.createElement('p');
+    pElement.innerText = 'You have been logged in successfully';
+    pElement.className.add('success');
+    document.body.appendChild(pElement);
+  }
+
+  function resetState(element) {
+    const smallElement = element.parentElement.querySelector('small');
+    smallElement.innerText = '';
+    element.classList.remove('invalid');
+    form.classList.remove('invalid');
+  }
+
+  function validateLength(element, min, max) {
+    const val = element.value;
+
+    if (val.length < min || val.length > max) {
+      const elementName = element.getAttribute('name');
+      displayError(element, `${elementName} length must be between ${min} and ${max}`)
+    }
+  }
+
+  function validateEmail(emailElement) {
+    const regex = /\S+@\S+\.\S+/;
+    if (regex.test(emailElement.value)) {
+      displayError(emailElement, 'Email must be valid');
+    }
+  }
+
+  function validateForm(event) {
+    event.preventDefault();
+
+    const emailElement = document.getElementById('email');
+    const passwordElement = document.getElementById('password');
+
+    resetState(emailElement);
+    resetState(passwordElement);
+
+    validateLength(emailElement, 10, 20);
+    validateLength(passwordElement, 8, 20);
+
+    validateEmail(emailElement);
+
+    const isValidForm = !form.classList.contains('invalid');
+    if (isValidForm) {
+      displaySuccess();
+    }
+  }
+  function run() {
+    const formElement = document.querySelector('form');
+    formElement.addEventListener('submit', validateForm)
+  }
+
+  run();
 })();
--- public/21-infinite-scroll-gallery/start.js
+++ public/21-infinite-scroll-gallery/start.js
@@ -1,3 +1,48 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  const KEY = 'eVDtoquGVkxTBxrngtYxEtyzuqgFdOfvW-yCwPEyVVs';
+  let page = 1;
+
+  const loaderElement = document.querySelector('.loader');
+  
+  function showLoader() {  
+    loaderElement.classList.add('visible');
+  }
+
+  function hideLoader() {
+    loaderElement.classList.remove('visible');
+  }
+
+  async function displayImages() {
+    showLoader();
+
+    const result = await fetch(`https://api.unsplash.com/photos/?client_id=${KEY}&page=${page}&query=food`);
+    const images = await result.json();
+
+    const galleryElement = document.querySelector('.gallery');
+
+    images.forEach(image => {
+      const imageElement = document.createElement('img');
+      imageElement.src = image.urls.small;
+      imageElement.alt = image.alt_description;
+      
+      galleryElement.appendChild(imageElement);
+    });
+
+    page += 1;
+    hideLoader();
+  }
+
+  function onScroll() {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 10) {
+      displayImages();
+    }
+  }
+
+  function run() {
+    document.addEventListener('scroll', onScroll);
+    displayImages();
+  }
+
+  run();
 })();
